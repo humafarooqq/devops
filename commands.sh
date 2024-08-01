0832f2c37ec483d5fd77d487ee27f79cc820ba48
root <root@Huma.thleoodkfk1ebavv1vutii4tjg.bx.internal.cloudapp.net>
2024-08-01 09:45:51 +0000

    second commit

---
+++ commands.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+#
+#to chexk if git is set or not
+#
+#
+git remote -v
+git branch main # creates a branch main
+git checkout main #switches to branch main
+git push -u origin main
+
+
+
